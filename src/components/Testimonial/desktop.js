969a47eef292740c28675e8f50706828c0119019
Bartosz Makos <bartosz.makos@xwp.co>
2022-10-24 13:19:22 +0200

    add: testimonials for company page / refactor from dev-statement

---
+++ src/components/Testimonial/desktop.js
@@ -0,0 +1,35 @@
+import {timeline} from 'motion'
+import stagger from '../../scripts/stagger.js'
+import observe from '../../scripts/viewObserver.js'
+
+const parent = document.querySelector('[data-name="testimonial"]')
+const contents = parent.innerHTML
+
+export function create () {
+  console.log(name)
+  const caption = document.querySelector('.testimonial__caption')
+  const overlay = caption.querySelector('.testimonial__caption-overlay')
+
+  observe(caption, {
+    on: () => setTimeout(() => overlay.style.transform = 'translateX(200%)', 500),
+    off: () => overlay.style.transform = 'translateX(0)',
+  })
+
+  const tl = timeline([
+    ['.testimonial', {position: 'fixed', top: 0, left: 0}, {duration: 0, at: 0}],
+    ['.testimonial', {transform: ['translateY(50%)', 'translateY(0vh)', 'translateY(-25%)']}, {duration: 2, at: 0}],
+    ['.testimonial', {opacity: [0,  1, 0], zIndex: [0 , 1, 0]}, {duration: 1, at: .5}],
+  ], {easing: 'cubic-bezier(1,1,0,0)'})
+
+  tl.pause()
+
+  stagger.onStageProgress('testimonial', p => {
+    tl.currentTime = p
+  }, true)
+}
+
+export function destroy () {
+  stagger.removeStageProgress('testimonial')
+  parent.innerHTML = contents
+}
+
